336c4e84e7cf6f1bafee5b4241a649b18f3827e2
b1nnnnid <suebenny2@gmail.com>
2025-05-16 21:56:08 +0900

    [0516] 프로젝트 연결

---
+++ app/src/main/java/com/example/momapp/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.momapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
